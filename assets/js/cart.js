81e3106e6291e11518fa8113f8a001bdc5bbcc61
Lwazi Ncube <lwazincubex@gmail.com>
2026-03-13 22:53:59 +0200

    minor update

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,19 +1,27 @@
-function addToCart(name, price) {
+function addToCart(name, price, button) {
     let cart = JSON.parse(localStorage.getItem("cart")) || []
 
     let existingItem = cart.find(item => item.name === name)
 
     if(existingItem) {
         cart = cart.filter(item => item.name !== name)
-        alert(`${name} has been removed from cart`)
+        button.innerHTML = `
+      <img src="/assets/images/cart.svg" height="18">
+      Add to cart
+    `
     } else {
         cart.push({
             name: name,
             price: price,
             quantity: 1
         })
+
+        button.innerHTML = `
+      ✓ Added
+    `
         
-        alert(`${name} has been Added to cart, you can increase quantity in order page`)
+        // alert(`${name} has been Added to cart, you can increase quantity in order page`)
+        // yenza kube yi toast lapha
     }
 
     localStorage.setItem("cart", JSON.stringify(cart))
